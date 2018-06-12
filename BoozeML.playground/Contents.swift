6e06efbc461e57a2bf59c2ca15cae96f2cc52832
Collin DeWaters <ctdewaters@Collins-MacBook-Pro.local>
2018-06-12 10:40:22 -0400

    Added playground file.

---
+++ BoozeML.playground/Contents.swift
@@ -0,0 +1,7 @@
+import CreateMLUI
+
+//Create the image classifier.
+let boozeClassifier = MLImageClassifierBuilder()
+
+//Display the image classifier in the live view.
+boozeClassifier.showInLiveView()
